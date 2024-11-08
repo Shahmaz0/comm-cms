e1a4599fcecf6e57c8607327527ba1d3bf12c16d
shahmaz0 <shahmaansari8@gmail.com>
2024-11-09 01:13:50 +0530

    Added code

--- app/components/dashboard/EditForm.tsx
+++ app/components/dashboard/EditForm.tsx
@@ -1,5 +1,6 @@
+
 "use client"
-import { createProduct } from "@/app/actions";
+import { createProduct, editProduct } from "@/app/actions";
 import { UploadDropzone } from "@/app/lib/uploadthing";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -18,11 +19,27 @@ import { useState } from "react";
 import Image from "next/image";
 import { categories } from "@/app/lib/categories";
 import { SubmitButton } from "@/app/components/SubmitButtons";
+import { type $Enums } from "@prisma/client";
+ 
+interface iAppProps {
 
-export default function ProductCreateRoute() {
+    data: {
+        id: string;
+        name: string;
+        description: string;
+        status: $Enums.ProductStatus;
+        price: number;
+        images: string[];
+        category: $Enums.Category;
+        isFeatured: boolean;
+    }
+}
 
-    const [images, setImages] = useState<string[]>([]);
-    const [lastResult, action] = useFormState(createProduct, undefined)
+
+ export function EditForm({ data } : iAppProps) {
+
+    const [images, setImages] = useState<string[]>(data.images);
+    const [lastResult, action] = useFormState(editProduct, undefined)
     const [form, fields] = useForm({
         lastResult,
 
@@ -37,21 +54,23 @@ export default function ProductCreateRoute() {
     const handleDelete = (index: number) => {
         setImages(images.filter((_, i) => i !== index));
     };
+    
     return (
         <form id={form.id} onSubmit={form.onSubmit} action={action}>
+            <input type="hidden" name="productId" value={data.id} />
             <div className="flex items-center gap-4">
                 <Button variant="outline" size="icon" asChild>
                     <Link href="/dashboard/products">
                         <ChevronLeft />
                     </Link>
                 </Button>
-                <h1 className="text-xl font-semibold tracking-tight">New Product</h1>
+                <h1 className="text-xl font-semibold tracking-tight">Edit Product</h1>
             </div>
 
             <Card className="mt-5">
                 <CardHeader>
                     <CardTitle>Product Details</CardTitle>
-                    <CardDescription>In this form you can create your product</CardDescription>
+                    <CardDescription>In this form you can update your product</CardDescription>
                 </CardHeader>
                 <CardContent>
                     <div className="flex flex-col gap-6">
@@ -61,7 +80,7 @@ export default function ProductCreateRoute() {
                                 type="text" 
                                 key={fields.name.key}
                                 name={fields.name.name}
-                                defaultValue={fields.name.initialValue}
+                                defaultValue={data.name}
                                 className="w-full" 
                                 placeholder="product name"
                             />
@@ -73,7 +92,7 @@ export default function ProductCreateRoute() {
                             <Textarea 
                                 key={fields.description.key} 
                                 name={fields.description.name} 
-                                value={fields.description.initialValue} 
+                                defaultValue={data.description} 
                                 placeholder="Write your description right here..." 
                             />
                             <p className="text-sm text-red-500">{fields.description.errors}</p>
@@ -84,7 +103,7 @@ export default function ProductCreateRoute() {
                             <Input 
                                 key={fields.price.key}
                                 name={fields.price.name}
-                                defaultValue={fields.price.initialValue}
+                                defaultValue={data.price}
                                 type="number" 
                                 placeholder="$55"
                             />
@@ -96,7 +115,7 @@ export default function ProductCreateRoute() {
                             <Switch
                                 key={fields.isFeatured.key}
                                 name={fields.isFeatured.name}
-                                defaultValue={fields.isFeatured.initialValue}
+                                defaultChecked={data.isFeatured}
                             />
                             <p className="te xt-sm text-red-500">{fields.isFeatured.errors}</p>
 
@@ -107,7 +126,7 @@ export default function ProductCreateRoute() {
                              <Select 
                                 key={fields.status.key}
                                 name={fields.status.name}
-                                defaultValue={fields.status.initialValue}
+                                defaultValue={data.status}
                              >
                                 <SelectTrigger>
                                     <SelectValue placeholder="Select Status"></SelectValue>
@@ -128,7 +147,7 @@ export default function ProductCreateRoute() {
                             <Select 
                                 key={fields.category.key}
                                 name={fields.category.name} 
-                                defaultValue={fields.category.initialValue}
+                                defaultValue={data.category}
                              >
                                 <SelectTrigger>
                                     <SelectValue placeholder="Select Category"></SelectValue>
@@ -154,7 +173,7 @@ export default function ProductCreateRoute() {
                                 value={images} 
                                 key={fields.images.key} 
                                 name={fields.images.name}
-                                defaultValue={fields.images.initialValue as any}
+                                defaultValue={data.images}
                             />
                             {images.length > 0 ? (
                                 <div className="flex gap-5">
@@ -193,9 +212,9 @@ export default function ProductCreateRoute() {
                     </div>
                 </CardContent>
                 <CardFooter>
-                    <SubmitButton text="Create Product"/>
+                    <SubmitButton text="Edit Product" />
                 </CardFooter>
             </Card>
         </form>
     )
-}
+ }
